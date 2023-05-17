5f6a4089ed4dfd228728ed1b4c4db99cae64b889
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-05-17 18:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,28 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
+const lookup = {
+    'A': 'N','B': 'O','C': 'P','D': 'Q',
+    'E': 'R','F': 'S','G': 'T','H': 'U',
+    'I': 'V','J': 'W','K': 'X','L': 'Y',
+    'M': 'Z','N': 'A','O': 'B','P': 'C',
+    'Q': 'D','R': 'E','S': 'F','T': 'G',
+    'U': 'H','V': 'I','W': 'J','X': 'K',
+    'Y': 'L','Z': 'M', '?': '?', ',': ','
+  };
 
+  function rot13(encodedStr){
+    let outputString = "";
+  for(let i = 0;i<encodedStr.length;i++){
+    if(lookup[encodedStr[i]] === undefined){
+        outputString += encodedStr[i];
+    }else{
+        outputString += lookup[encodedStr[i]]
+    }
+  }
+  return outputString;
+  }
+module.exports = rot13;
   return ;//return decodedArr
 }
 
